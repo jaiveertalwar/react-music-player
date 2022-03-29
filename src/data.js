e6f6fc013b6317bb6e02ecf1e99906f87aa8504a
Jaiveer Talwar <72844076+jaiveertalwar@users.noreply.github.com>
2022-03-29 12:09:59 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ function chillHop() {
 		{
 			name: "Fluffy State of Mind",
 			cover:
-				"./public/poster.png",
+				"https://thepuppyhiphop.coa.pw/poster.png",
 			artist: "Cutest Coopster - The Puppy Hip Hop - Single",
 			audio: "https://dl.jtlr.cf/api/v1/posts/6242a7a1c113714d0b4b005a/download/6242a7a1c113718e9a4b005c",
 			color: ["#205950", "#2ab3bf"],
