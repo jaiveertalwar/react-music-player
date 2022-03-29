b7f5375d2520ac6918bba595ed9a65159bd28594
Jaiveer Talwar <72844076+jaiveertalwar@users.noreply.github.com>
2022-03-29 12:07:24 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ function chillHop() {
 		{
 			name: "Fluffy State of Mind",
 			cover:
-				"https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+				"./public/poster.png",
 			artist: "Cutest Coopster - The Puppy Hip Hop - Single",
 			audio: "https://dl.jtlr.cf/api/v1/posts/6242a7a1c113714d0b4b005a/download/6242a7a1c113718e9a4b005c",
 			color: ["#205950", "#2ab3bf"],
